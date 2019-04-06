2a3ffac2630cc1fcc40c1f34d2e4afbab52944d6
yamamoto1108 <kjrps0826@gmail.com>
2019-04-06 15:16:50 +0900

    Edit groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,28 @@
 class GroupsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		Group.create
+		redirect_to root_path
+	end
+
+	def edit
+	end
+
+	def update
+		if current_group.update(group_params)
+			redirect_to root_path
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def group_params
+		params.permit(:group).permit(:name)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update]
   root 'messages#index'
 end
