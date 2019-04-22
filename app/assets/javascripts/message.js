75003632249eb85b670c545a74d25d0646826cdc
yamamoto1108 <kjrps0826@gmail.com>
2019-04-22 21:09:26 +0900

    Set buildHTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,17 @@
 $(function() {
+  function buildHTML(message) {
+    var html = `.message
+                  %p.message__user
+                    = message.user.name
+                  %p.message__date
+                    = message.created_at.strftime("%Y/%m/%d %H:%M")
+                  %p.message__text
+                    = message.body
+                    %br
+                    = image_tag message.image.url if message.image.present?`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
