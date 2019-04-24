5af715b4861562bb4c22eae46e477fd50e5f9c0b
yamamoto1108 <kjrps0826@gmail.com>
2019-04-24 15:14:02 +0900

    Add Ajax

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,12 @@
 $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+
+    $.ajax({
+      type: 'GET',
+      url: 'users/index',
+      data: { keyword: input },
+      datatype: 'json'
+    })
   });
 });
