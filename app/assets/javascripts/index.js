f84f73169729f71ce0d75c5e6653df5f82495f66
yamamoto1108 <kjrps0826@gmail.com>
2019-04-24 15:30:15 +0900

    Set done action

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -8,5 +8,16 @@ $(function() {
       data: { keyword: input },
       datatype: 'json'
     })
+
+    .done(function(users) {
+      $("#user-search-result").empty()  //検索結果の削除
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          apeendUser(user);
+        });
+      } else {
+        appendErrMsgToHTML("一致するユーザーがありません");
+      }
+    })
   });
 });
