30f60af9fbc65c8520bea5ca66b41711d8311a53
yamamoto1108 <kjrps0826@gmail.com>
2019-04-24 13:30:40 +0900

    fix indent

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
-	belongs_to :group
-	belongs_to :user
-	validates :body, presence: true, unless: :image?
-	mount_uploader :image, ImageUploader
+  belongs_to :group
+  belongs_to :user
+  validates :body, presence: true, unless: :image?
+  mount_uploader :image, ImageUploader
 end
