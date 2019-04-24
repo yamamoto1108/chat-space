e5b71c8af2e3e70a803fccd76ea10bcae7661a24
yamamoto1108 <kjrps0826@gmail.com>
2019-04-24 10:03:51 +0900

    Set ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,38 +1,53 @@
 $(function() {
-  function buildHTML(message) {
-    var html = `.message
-                  %p.message__user
-                    = message.user.name
-                  %p.message__date
-                    = message.created_at.strftime("%Y/%m/%d %H:%M")
-                  %p.message__text
-                    = message.body
-                    %br
-                    = image_tag message.image.url if message.image.present?`
-    return html;
-  }
-
-  $('#new_message').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html)
-      $('.box__text').val('')
-    })
-
-    .fail(function(){
-      alert('error');
-    })
+  $(document).on('turbolinks:load', function () {
+    function buildHTML(message) {
+      var img = ""
+      console.log(message.image)
+        if (message.image !== null) {
+          img = `<img src ="${ message.image.url }"></p>`
+        } else {
+          img = `</p>`
+        }
+      var html = `<div class="message">
+                    <p class="message__user">
+                      ${ message.user_name }
+                    </p>
+                    <p class="message__date">
+                      ${ message.created_at }
+                    </p>
+                    <p class="message__text">
+                      ${ message.body }
+                      <br>
+                      ${ img }
+                  </div>`
+      return html;
+    }
+ 
+    $('#new_message').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      // console.log(url)
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+  
+      .done(function(data){
+        // console.log(data)
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $('.box__text').val('')
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}); 
+      })
+  
+      .fail(function(){
+        alert('error');
+      })
+    });
   });
 });
