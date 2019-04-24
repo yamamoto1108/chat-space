a059ef59d675972e6efa93d5848fc48ce1a0f3e6
yamamoto1108 <kjrps0826@gmail.com>
2019-04-24 19:08:01 +0900

    Add html for search result

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,43 +1,45 @@
 $(function() {
+  $(document).on('turbolinks:load', function () {
 
-  var search_list = $("#user-search-result");
+    var search_list = $("#user-search-result");
 
-  function appendUser(user) {
-    var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${ user.name }</p>
-                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
-                </div>`
-    search_list.append(html)
-  }
+    function appendProduct(user) {
+      var html = `<div class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${ user.name }</p>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                  </div>`
+      search_list.append(html)
+    }
 
-  function appendErrMsgToHTML(msg) {
-    var html = `<div class="chat-group-user clearfix">${ msg }</div>`
-    search_list.append(html);
-  }
+    function appendErrMsgToHTML(msg) {
+      var html = `<div class="chat-group-user clearfix">${ msg }</div>`
+      search_list.append(html);
+    }
 
-  $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
+    $("#user-search-field").on("keyup", function() {
+      var input = $("#user-search-field").val();
+      
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
 
-    $.ajax({
-      type: 'GET',
-      url: 'users/index',
-      data: { keyword: input },
-      datatype: 'json'
-    })
+      .done(function(users) {
+        $("#user-search-result").empty()  //検索結果の削除
+        if (users.length !== 0) {
+          users.forEach(function(user) {
+            appendProduct(user);
+          });
+        } else {
+          appendErrMsgToHTML("一致するユーザーがありません");
+        }
+      })
 
-    .done(function(users) {
-      $("#user-search-result").empty()  //検索結果の削除
-      if (users.length !== 0) {
-        users.forEach(function(user) {
-          apeendUser(user);
-        });
-      } else {
-        appendErrMsgToHTML("一致するユーザーがありません");
-      }
-    })
-
-    .fail(function() {
-      alert('ユーザー検索に失敗しました');
-    })
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    });
   });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
-      format.html
+      format.html { redirect_to new_group_path }
       format.json
     end
+    
   end
 
   def edit
