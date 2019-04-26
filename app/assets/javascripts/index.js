f8e1e3eb474d5b585727612cb24d5d2fd795249c
yamamoto1108 <kjrps0826@gmail.com>
2019-04-26 21:15:41 +0900

    Add chat member list

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,13 +2,23 @@ $(function() {
   $(document).on('turbolinks:load', function () {
 
     var search_list = $("#user-search-result");
+    var selected_list = $("#chat-group-users");
 
-    function appendProduct(user) {
-      var html = `<div class="chat-group-user clearfix">
+    function appendList(user) {
+      var html = `<div class="chat-group-user clearfix search">
                     <p class="chat-group-user__name">${ user.name }</p>
-                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                    <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" id="${ user.id }" data-user-name="${ user.name }">追加</div>
                   </div>`
       search_list.append(html)
+    }
+
+    function appendUser(user) {
+      var html = `<div class='chat-group-user clearfix js-chat-member' id='${ user.name }'>
+                    <input name='group[user_ids][]' type='hidden' value='${ user.name }'>
+                    <p class='chat-group-user__name'>${ user.name }</p>
+                    <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' id='${ user.name }'>削除</div>
+                  </div>`
+      selected_list.append(html)
     }
 
     function appendErrMsgToHTML(msg) {
@@ -27,11 +37,22 @@ $(function() {
       })
 
       .done(function(users) {
-        $("#user-search-result").empty()  //検索結果の削除
+        $("#user-search-result").empty()
         if (users.length !== 0) {
           users.forEach(function(user) {
-            appendProduct(user);
+            appendList(user);
+            
+            $(".chat-group-user.clearfix.search").on("click", `#${user.id}`, function() {
+              appendUser(user);
+              $(".chat-group-user.clearfix.search").remove();
+            });
+
+            $(".chat-group-user.clearfix.js-chat-member").on("click", `#${user.name}`, function() {
+              $(`#${user.name}`).remove();
+            });
+            
           });
+          
         } else {
           appendErrMsgToHTML("一致するユーザーがありません");
         }
