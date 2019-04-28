b55b1201902fe6d19fcd9c31b00e537d4cf6160a
yamamoto1108 <kjrps0826@gmail.com>
2019-04-28 12:03:31 +0900

    Edit input value

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -14,7 +14,7 @@ $(function() {
 
     function appendUser(user) {
       var html = `<div class='chat-group-user clearfix js-chat-member' id='${ user.id }b' >
-                    <input name='group[user_ids][]' type='hidden' value='${ user.name }'>
+                    <input name='group[user_ids][]' type='hidden' value='${ user.id }'>
                     <p class='chat-group-user__name'>${ user.name }</p>
                     <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn' id='${ user.id }c'>削除</div>
                   </div>`
