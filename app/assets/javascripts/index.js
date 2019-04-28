a9887f59ba46a6ae81d3a3a5a653954e5ead9419
yamamoto1108 <kjrps0826@gmail.com>
2019-04-28 14:38:37 +0900

    Add ;

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -37,7 +37,7 @@ $(function() {
       })
 
       .done(function(users) {
-        $("#user-search-result").empty()
+        $("#user-search-result").empty();
         if (users.length !== 0) {
           users.forEach(function(user) {
             appendList(user);
