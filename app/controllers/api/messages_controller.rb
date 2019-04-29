2a42827ba7decf65640270e5365b1a0d1ec9f368
yamamoto1108 <kjrps0826@gmail.com>
2019-04-29 12:57:12 +0900

    Fix @group.messages

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,8 +1,9 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
     respond_to do |format|
       format.html
-      format.json{@messages = Message.where('id > ?', params[:id])}
+      format.json{@messages = @group.messages.where('id > ?', params[:id])}
     end
   end
 end
